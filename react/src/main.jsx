f862ea7b06f7d132acc846d89d777a19945b0a32
khanhchi323 <npkc323@npkc323gmail.com>
2024-01-15 17:45:06 +0700

    add filee

--- react/src/main.jsx
+++ react/src/main.jsx
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom';
 import './index.css'
 import App from './App';
-import MenuBar from './components/MenuBar';
+import MenuBar from './components/public/MenuBar';
+import Footer from './components/public/footer';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
   <MenuBar/>
     <App />
-    
+    <Footer/>
   </BrowserRouter>,
 );
