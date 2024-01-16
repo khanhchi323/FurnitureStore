25fe476022fb505502e0de529d552f4730a87b0d
Duongbang194 <nguyenduongbang194@gmail.com>
2024-01-16 17:33:43 +0700

    productdetail-editprofile

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -5,23 +5,21 @@ import HomePage from "./views/public/HomePage";
 import LoginPage from "./views/public/LoginPage";
 import RegisterPage from "./views/public/RegisterPage";
 import ProfilePage from "./views/public/ProfilePage";
-// import EditProfilePage from "./views/public/EditProfilePage";
+import EditProfilePage from "./views/public/EditProfilePage";
 import ProductPage from "./views/public/ProductPage";
-// import ProductDetailPage from "./views/public/ProductDetailPage";
-
+import ProductDetailPage from "./views/public/ProductDetailPage";
 export default function App() {
 
   return (
     <div>
       <Routes>
-        <Route path='/' element={<HomePage/>}></Route>
-        <Route path='/login' element={<LoginPage/>}></Route>
-        <Route path='/register' element={<RegisterPage/>}></Route>
-        <Route path='/profile' element={<ProfilePage/>}></Route>
-        {/* <Route path='/edit-profile' element={<EditProfilePage/>}></Route> */}
-        <Route path='/product' element={<ProductPage/>}></Route>
-        {/* <Route path='/product-detail' element={<ProductDetailPage/>}></Route> */}
-
+        <Route path='/' element={<HomePage />}></Route>
+        <Route path='/login' element={<LoginPage />}></Route>
+        <Route path='/register' element={<RegisterPage />}></Route>
+        <Route path='/profile' element={<ProfilePage />}></Route>
+        <Route path='/edit-profile' element={<EditProfilePage />}></Route>
+        <Route path='/product' element={<ProductPage />}></Route>
+        <Route path='/product-detail' element={<ProductDetailPage />}></Route>
       </Routes>
     </div>
   );
---
+++ react/src/views/public/ProductDetailPage.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+export default function ProductDetailPage() {
+    return (
+        <div className="font-sans m-0 p-0  bg-white h-screen w-full ">
+            <div className="bg-gray-300 h-12 text-2xl  ">
+                <div className="p-auto pt-1.5 text-center lg:text-left md:text-left lg:pl-3 md:pl-3 ">Product Detail</div>
+            </div>
+            <div className="flex flex-col lg:flex-row mt-8 md:flex-row items-center justify-center align-center">
+                <div className="lg:w-1/2 md:w-1/2  ">
+                    <img className="sm:w-64 sm:text-center m-auto h-56 w-64  lg:w-3/4 lg:h-full md:w md:h-full rounded-xl " src={Intro8} alt="Hình sản phẩm" />
+
+                </div>
+
+                <div className="lg:w-1/2 md:w-1/2 justify-items-center text-left lg:text-center lg:text-justify">
+                    <div className="px-8 sm:px-12 ">
+                        <h1 className="lg:text-3xl md:text-2xl text-xl px-0.5">Name product Name product</h1>
+                        <div className="flex items-center">
+                            <span className="lg:text-xl line-through text-gray-500 text-sm">1.000.000đ</span>
+                            <span className=" lg:text-3xl ml-1.5 mr-2 font-bold text-black-500 text-2xl">900.000đ</span>
+                            <span className="p-0.5 bg-red-700 text-xs text-white rounded px-1">giảm 10%</span>
+                        </div>
+                        <span className="my-3  ">Mô tả sản phẩm</span>
+                        <div className="flex flex-row" >
+                            <div className="w-1/3  mt-2	 ">
+                                <button className="w-1/5 border border-black  " onclick="decrementQuantity()">-</button>
+                                <input className="w-1/5 text-center  border border-black   " type="text" value="1" min="1" />
+                                <button className="w-1/5  border border-black " onclick="incrementQuantity()">+</button>
+                            </div>
+                            <div className="w-1/3  text-center hover:bg-slate-700 py-2 px-0.5 hover:text-white text-base text-center bg-white text-gray-700 border-gray-600 border  ">
+                                <button className="">Add to cart</button>
+                            </div>
+                        </div>
+                        <div className="my-6 ">SKU-</div>
+                        <div className="my-6 ">Category:</div>
+                        <div className="w-3/4 ">
+                            <p className="">Chi tiết sản phẩm</p>
+                            <p className="">TextTextText Text Text Text Text Text    Text Text Text Text Text Text Text Text Tex</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div >
+    );
+}
