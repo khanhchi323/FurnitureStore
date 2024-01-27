f5a356e0bc32dba684fe8c32e8f2bc43f3dced81
khanhchi323 <npkc323@npkc323gmail.com>
2024-01-27 16:54:42 +0700

    navbar and sidebar in Admin page

--- react/src/components/admin/Navbar.jsx
+++ react/src/components/admin/Navbar.jsx
@@ -3,14 +3,14 @@ import { Badge, IconButton, Avatar } from "@material-tailwind/react";
 import { ChatBubbleOvalLeftEllipsisIcon } from "@heroicons/react/24/solid";
 export default function Navbar() {
     return (
-        <div className="mx-auto max-w-screen-3xl p-2 lg:rounded-xl lg:pl-6 flex justify-between ">
+        <div className="mx-auto max-w-screen-3xl mt-8 mr-8 lg:rounded-xl lg:pl-6 flex justify-between ">
             <div>
                 <b>CCHLB</b>
             </div>
 
             <div className="flex items-center gap-8">
-                <Badge content="3">
-                    <IconButton>
+                <Badge className=" shadow-none" content="3">
+                    <IconButton  color = "white" size = "circular">
                         <ChatBubbleOvalLeftEllipsisIcon className="h-4 w-4" />
                     </IconButton>
                 </Badge>
--- react/src/main.jsx
+++ react/src/main.jsx
@@ -11,16 +11,16 @@ import ProfilePage from "./views/public/ProfilePage";
 import EditProfilePage from "./views/public/EditProfilePage";
 import ProductPage from "./views/public/ProductPage";
 import ProductDetailPage from "./views/public/ProductDetailPage";
-import AboutUsPage from "./views/public/AboutUsPage"
-import CartPage from "./views/public/CartPage"
+import AboutUsPage from "./views/public/AboutUsPage";
+import CartPage from "./views/public/CartPage";
 import EditAddress from "./views/public/EditAddress";
-import ComfirmationOrder from "./views/public/ComfirmationOrder"
+import ComfirmationOrder from "./views/public/ComfirmationOrder";
 import PaymentMethods from "./views/public/PaymentMethods";
 import ContactUs from "./views/public/ContactUs";
 import OrderHistory from "./views/public/OrderHistory";
 import Error404 from "./views/public/Error404";
 import SideBar from "./components/admin/SideBar";
-import Nav from "./components/admin/Navbar"
+import Navbar from "./components/admin/Navbar";
 
 const User = () => {
     return (
@@ -33,43 +33,49 @@ const User = () => {
 };
 
 const Admin = () => {
-    return <div>
-    <Nav/>
-      {/* <SideBar/> */}
-    </div>;
+    return (
+        <div className="flex flex-col h-screen relative ">
+            <div className="fixed bg-gray-100 w-full h-full -z-10"></div>
+            <div className=" top-0 z-10 ">
+                <Navbar />
+            </div>
+
+            <div className="flex flex-grow md:flex-row pr-2">
+                <div className="hidden md:block md:w-auto static left-0 bottom-0">
+                    <SideBar />
+                </div>
+                <Outlet/>
+            </div>
+        </div>
+    );
 };
 ReactDOM.createRoot(document.getElementById("root")).render(
-    
-            <BrowserRouter>
-                <Routes>
-                    {/* ERROR 404 */}
-                    <Route path="*" element={<Error404 />} />
+    <BrowserRouter>
+        <Routes>
+            {/* ERROR 404 */}
+            <Route path="*" element={<Error404 />} />
 
-                    {/* PUBLIC */}
-                    <Route path="/" element={<User />}>
-                        <Route index element={<HomePage />} />
-                        <Route path="/login" element={<LoginPage />} />
-                        <Route path="/register" element={<RegisterPage />} />
-                        <Route path="/profile" element={<ProfilePage />} />
-                        <Route path="/edit-profile" element={<EditProfilePage />}/>
-                        <Route path="/product" element={<ProductPage />} />
-                        <Route path="/product-detail" element={<ProductDetailPage />} />
-                        <Route path="/about-us" element={<AboutUsPage />} />
-                        <Route path="/cart-page" element={<CartPage />} />
-                        <Route path="/cre-add" element={<EditAddress />} />
-                        <Route path="/confirmation-order"element={<ComfirmationOrder />}/>
-                        <Route path="/contactus" element={<ContactUs />} />
-                        <Route path="/order-history" element={<OrderHistory />}/>
-                        
-                    </Route>
+            {/* PUBLIC */}
+            <Route path="/" element={<User />}>
+                <Route index element={<HomePage />} />
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/register" element={<RegisterPage />} />
+                <Route path="/profile" element={<ProfilePage />} />
+                <Route path="/edit-profile" element={<EditProfilePage />} />
+                <Route path="/product" element={<ProductPage />} />
+                <Route path="/product-detail" element={<ProductDetailPage />} />
+                <Route path="/about-us" element={<AboutUsPage />} />
+                <Route path="/cart-page" element={<CartPage />} />
+                <Route path="/cre-add" element={<EditAddress />} />
+                <Route
+                    path="/confirmation-order"
+                    element={<ComfirmationOrder />}
+                />
+                <Route path="/contactus" element={<ContactUs />} />
+                <Route path="/order-history" element={<OrderHistory />} />
+            </Route>
 
-                    <Route  path="/admin" element={<Admin/>}>
-                    
-
-                    </Route>
-                </Routes>
-
-                
-            </BrowserRouter>
-
+            <Route path="/admin" element={<Admin />}></Route>
+        </Routes>
+    </BrowserRouter>
 );
