97fa183e552857f8fe0d3ec29b86c6d9dd69290c
Chuong16082003 <nguyenchuong10c4@gmail.com>
2024-01-30 13:42:10 +0700

    Chuong thay doi

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -9,6 +9,6 @@ class Product extends Model
 {
     use HasFactory;
     protected $table = 'product';
-    protected $fillable = ['product_id', 'discount_id','product_category_id','product_status_id','product_name','description','image','quantity','price','created_at','updated_at','deleted_at'];
+    protected $fillable = ['product_id', 'discount_id','product_category_id','product_status_id','product_name','color','description','image','quantity','price','created_at','updated_at','deleted_at'];
     protected $primaryKey = 'product_id';
 }
