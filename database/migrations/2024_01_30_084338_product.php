27bd90976dd3a5964544afb98920e590e032ceb6
Chuong16082003 <nguyenchuong10c4@gmail.com>
2024-02-07 10:08:46 +0700

    Chuong thay doi

--- database/migrations/2024_01_30_084338_product.php
+++ database/migrations/2024_01_30_084338_product.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('product',function(Blueprint $table){
             $table->bigIncrements('product_id');
             $table->unsignedBigInteger('product_category_id');
-            $table->unsignedBigInteger('color');
+            $table->string('color');
             $table->string('product_name',50);
             $table->string('description',500);
             $table->string('image');
